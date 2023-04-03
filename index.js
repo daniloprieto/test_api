04510fa3ef9fc52a4afd5c9da8429283738854fb
Danilo Prieto <danilo.s.prieto@gmail.com>
2023-04-03 01:45:55 -0300

    Add middleware cors

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const routerApi = require('./routes');
+const cors = require('cors');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+// const whiteList = ['http://localhost:5555'];
+// const options = {
+//     origin: (origin, callback) => {
+//         if(whiteList.includes(origin)){
+//             callback(null, true)
+//         }else{
+//             callback(new Error(`Not allowed`));
+//         }
+//     }
+// }
+// app.use(cors(options)) // Solo acceden orifenes en el array
+app.use(cors()) // todos los origenes
 
 routerApi(app);
 
